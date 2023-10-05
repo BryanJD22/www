b263fb0f805ca45b431d43c53a36ef78d7be04ed
Bryan J <bryandavila520@gmail.com>
2023-10-05 12:10:54 +0200

    Update C_Usuarios.php

--- C_Usuarios.php
+++ C_Usuarios.php
@@ -3,6 +3,23 @@
         public function __construct(){
             parent::__construct();
         }
+
+        public function validarUsuario($datos){
+            $usuario='';
+            $pass = 'srfdvsvwrt';
+            extract($datos);
+            $valido='N';
+            if ($usuario=='javier' && $pass=='') {
+                $mensa = 'Debe completar los campos';
+        
+            }else{
+                require_once 'controladore/C_Usuarios.php';
+                $objUsuarios = new C_Usuarios();
+
+
+            }
+        }
+
     }
 
 ?>
